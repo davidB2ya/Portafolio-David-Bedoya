1968d039f88ee5dd06d7cea117d481b48a398389
davidB2ya <dbedoya0408@gmail.com>
2022-04-05 13:49:54 -0500

    Modify the folder structure

---
+++ english/dataEn.js
@@ -0,0 +1,121 @@
+const data =  [
+    {
+        "id": 1,
+        "name": "Tetrikids",
+        "description": 'A classic of the games: "Tetris" with two new pieces; discover them ...',
+        "img": "https://i.ibb.co/z79Zrht/Tetrikids.jpg",
+        "deploy": "https://61231419c3f34400087d7cce--tetrikids.netlify.app/",
+        "github": "https://github.com/davidB2ya/Tetris-Colaborativo",
+        "stacks": ["HTML5","CSS","JavaScript"]
+    },
+    {
+        "id": 2,
+        "name": "Landing Page Lucero Hyla",
+        "description": "Website of the Commercial Executive Lucero Devia of the Hyla company in Colombia",
+        "img": "https://i.ibb.co/wN86sKp/Landing-Page-Lucero-Hyla.jpg",
+        "deploy": "https://landing-page-lucero-hyla.vercel.app/",
+        "github": "https://github.com/davidB2ya/Landing-Page-Lucero-Hyla",
+        "stacks": ["HTML5","CSS","JavaScript","React"] 
+    },
+    {
+        "id": 3,
+        "name": "JavaScript projects",
+        "description": "Project where I show a gallery of JavaScript challenges",
+        "img": "https://i.ibb.co/R255VBm/Retos-JS.jpg",
+        "deploy": "https://retos-js-seven.vercel.app/",
+        "github": "https://github.com/davidB2ya/retos-js",
+        "stacks": ["HTML5", "CSS", "JavaScript", "React"]
+    },
+    {
+        "id": 4,
+        "name": "Movie Club CRUD",
+        "description": "A CRUD for a Video Club",
+        "img": "https://i.ibb.co/9bj3wZZ/Movie-Club-CRUD.jpg",
+        "deploy": "https://crud-angular-colaborativo.herokuapp.com/home",
+        "github": "https://github.com/davidB2ya/Video-Club-CRUD-Angular",
+        "stacks": ["HTML5", "CSS", "JavaScript", "Angular","MySQL","API-Rest"]
+    },
+    {
+        "id": 5,
+        "name": "Merca Todo",
+        "description": "Website for a Market, which needed to organize and manage its product inventory through a CRUD",
+        "img": "https://i.ibb.co/72KRyL0/Merca-Todo.jpg",
+        "deploy": "https://merca-todo-1cyjty93y-merca-todo.vercel.app/",
+        "github": "https://github.com/davidB2ya/Merca-Todo/tree/master",
+        "stacks": ["HTML5", "CSS", "JavaScript","React","Django","Bootstrap","MySQL"]
+    },
+    {
+        "id": 6,
+        "name": "Chat Intopcol",
+        "description": "A business chat dedicated to surveying",
+        "img": "https://i.ibb.co/mT93h6G/Chat-Intop-Col.jpg",
+        "deploy": "https://mysterious-peak-94476.herokuapp.com/",
+        "github": "https://github.com/davidB2ya/Chat-Intopcol/tree/main",
+        "stacks": ["HTML5", "CSS", "JavaScript", "React", "Node.js","Express.js"]
+    },
+    {
+        "id": 7,
+        "name": "Memorama",
+        "description": "A memory game that I did to teach a group of students the fundamentals of Web programming",
+        "img": "https://i.ibb.co/mcKNQys/Memorama.jpg",
+        "deploy": "https://memorama-seven.vercel.app/",
+        "github": "https://github.com/davidB2ya/Memorama",
+        "stacks": ["HTML5", "CSS", "JavaScript"]
+    },
+    {
+        "id": 8,
+        "name": "Movie Cards",
+        "description": "A study app to learn how to use API-Rest and React",
+        "img": "https://i.ibb.co/yBmGnf5/Movie-Cards.jpg",
+        "deploy": "https://movie-cards-two.vercel.app/",
+        "github": "https://github.com/davidB2ya/Movie-Cards",
+        "stacks": ["HTML5", "CSS", "JavaScript","React","API-Rest"]
+    },
+    {
+        "id": 9,
+        "name": "Tribute Web",
+        "description": "Website to pay tribute to singer-songwriter Silvio Rodriguez",
+        "img": "https://i.ibb.co/cDhP5pK/Silvio.jpg",
+        "deploy": "./siteConstructionEn.html",
+        "github": "",
+        "stacks": ["HTML5","CSS","JavaScript"]
+    },
+    {
+        "id": 10,
+        "name": "Game Club ",
+        "description": "Website in which I use the RAWG API and tools like TailwindCSS.",
+        "img": "https://i.ibb.co/D472FwC/Game-Club-B2yas.jpg",
+        "deploy": "https://enigmatic-crag-17140.herokuapp.com",
+        "github": "https://github.com/davidB2ya/Video-Juegos-Club.git",
+        "stacks": ["HTML5","CSS","JavaScript","React","API-Rest","Node.js","Express.js","Mongo DB","TailwindCSS"]
+    },
+    {
+        "id": 11,
+        "name": "Mentoring App 'Okhlos'",
+        "description": "Application that is part of the ecosystem created by Prográmte for the development of BootCamp",
+        "img": "https://i.ibb.co/bm5bfKX/Okhlos.jpg",
+        "deploy": "https://fathomless-bastion-33135.herokuapp.com/",
+        "github": "https://github.com/davidB2ya/MentoringApp.git",
+        "stacks": ["HTML5","CSS","JavaScript","React","Node.js","Express.js","Mongo DB"]
+    },
+    {
+        "id": 12,
+        "name": "Coin Exchanges",
+        "description": "Application to quote the value in real time of the 100 most important cryptocurrencies",
+        "img": "https://i.ibb.co/fqsR0Vz/Coin-Exchanges-B2ya-1.jpg",
+        "deploy": "http://coin-exchanges-b2ya.herokuapp.com",
+        "github": "https://github.com/davidB2ya/Coin-Exchanges",
+        "stacks": ["HTML5","CSS","JavaScript","React","Node.js","Express.js","Mongo DB","Sass"]
+    },
+    {
+        "id": 13,
+        "name": "El Horno",
+        "description": "Site to order pizzas both personalized and from the menu",
+        "img": "https://i.ibb.co/vqZ4cpW/El-Horno.jpg",
+        "deploy": "https://el-horno.vercel.app",
+        "github": "https://github.com/davidB2ya/El-Horno",
+        "stacks": ["HTML5","CSS","JavaScript","React","Node.js","Express.js","Mongo DB"]
+    }
+]
+
+export default data
